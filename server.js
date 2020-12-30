4c689f8a70405e564f10022f838fa531fb04049f
girish <girishgodage@gmail.com>
2020-12-30 19:49:59 +0530

    Heroku deployed changes

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/covid19-tracker'))
+app.use(express.static(__dirname + '/dist/ngCovid19App'))
 
 app.get('/*',function(req,res){
-  res.sendFile(path.join(__dirname + '/dist/covid19-tracker/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ngCovid19App/index.html'));
 });
 
 // start the app by listening on the default port
